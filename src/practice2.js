6b1af366cecb316d66cd041bd6342ec7c1be75b3
mizhidan <dj9824@126.com>
2020-09-13 19:30:14 +0800

    [danjie] feat: test 2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
+const inject = (item, sections) => {
+    const result = [];
+    item.forEach((element, elementIndex) => {
+        sections.forEach(section => {
+            if(section.index === elementIndex) {
+                result.push(section.content);
+            }
+        })
+        result.push(element);
+    });
+    return result;
+}
 export { inject };
