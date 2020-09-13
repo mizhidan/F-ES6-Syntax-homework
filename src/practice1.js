20a10950e6c59ed6974422e2a9dc5649f8df8252
mizhidan <dj9824@126.com>
2020-09-13 18:27:26 +0800

    [danjie] feat: test 1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData = (input) => {
+    const result = []
+    input.data.forEach(array => {
+        let columnObj = {}
+        let arrayIndex = 0
+        input.column.forEach(obj => {
+            columnObj[obj.name] = array[arrayIndex]
+            arrayIndex++
+        })
+        result.push(columnObj)
+    })
+    return result;
+}
+
+
 export { parseData };
