aee469dc0805a8c77a922f8a5822e91fb2f0f5d7
kevindongzg <dongkevin94@gmail.com>
2020-09-21 21:29:50 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,7 +1,10 @@
+// TODO 使用解构的方式取出data与column
 const parseData = (input) => {
+    // TODO 使用map做数组与数组的转化，不用声明result
     const result = []
     input.data.forEach(array => {
         let columnObj = {}
+        // TODO 不需要单独声明index, 可以查一下forEach内函数的第二个参数就是index
         let arrayIndex = 0
         input.column.forEach(obj => {
             columnObj[obj.name] = array[arrayIndex]
--- src/practice2.js
+++ src/practice2.js
@@ -1,8 +1,11 @@
 const inject = (item, sections) => {
+    // TODO 数组与数组之间转化用map
     const result = [];
     item.forEach((element, elementIndex) => {
         sections.forEach(section => {
             if(section.index === elementIndex) {
+                // TODO 嵌套太多，想想有没有减少嵌套的办法
+                // TODO 查一查数组的splice方法
                 result.push(section.content);
             }
         })
